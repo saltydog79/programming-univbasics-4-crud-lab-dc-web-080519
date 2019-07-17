c27525861f0d6db540c3dbd179f48da996c66d32
Emily <aspentree79@gmail.com>
2019-07-17 12:31:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,37 +1,39 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+fruits = ["banana", "apple", "mango", "blueberries"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+dog_names = ["Scout", "Gus"]
+dog_names.push("Ranger")
+p dog_names
 end
 
 def add_element_to_start_of_array(array, element)
-  
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
 
 def update_element_from_index(array, index_number, element)
