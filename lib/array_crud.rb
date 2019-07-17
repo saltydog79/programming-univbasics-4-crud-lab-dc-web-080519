86866affc95d77670db3bbdc6d8be0b4f73b0208
Emily <aspentree79@gmail.com>
2019-07-17 12:32:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,7 @@ fruits = ["banana", "apple", "mango", "blueberries"]
 end
 
 def add_element_to_end_of_array(array, element)
-dog_names = ["Scout", "Gus"]
-dog_names.push("Ranger")
-p dog_names
+array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
